6c2b67f829ff1d42cb6800992899681c96e64db7
CHOI DAE GEON <chleorjs37@gmail.com>
2022-05-29 18:22:30 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="conatainer">
+    <div class="title">
+      <h1>
+        Slot
+      </h1>
+    </div>
+    
+    <ChildComponent :paraentList="testData">
+      <template #thead>
+          <th>출장비 내역</th>
+          <th>출장비 내역</th>
+          <th>금액</th>
+      </template>
+
+      <template #list="{ row }">
+        <td @click="app.click(row)">{{ row.item }}</td>
+				<td>{{ row.item2 }}</td>
+				<td>{{ row.item3 }}</td>
+      </template>
+    </ChildComponent>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive, ref } from 'vue';
+import ChildComponent from './components/ChileComponent.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    ChildComponent
+  },
+  setup(){
+    const testData = ref(
+      [
+        { item:1, item2:2,item3:3},
+        { item:1, item2:2,item3:3},
+        { item:1, item2:2,item3:3},
+      ]
+    )
+
+    const app = reactive(
+      {
+        click: (item)=>{
+          alert(`Click : ${item.item}`)
+        }
+      }
+    )
+    
+    return{
+      testData,
+      app
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*{
+  margin: 0;
+  padding: 0;
+}
+.conatainer{
+  max-width: 1920px;
+  width: 100%;
+  margin: 0 auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #eee;
 }
 </style>
---
+++ src/components/ChileComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <slot name="thead"></slot>
+            </tr>
+        </thead>
+        <tbody>
+            <template v-for="(item,index) in paraentList" :key="'item'+index">
+                <tr>
+                    <slot name="list" :row="item"></slot>
+                </tr>
+            </template>
+            <tr v-if="paraentList.length === 0">
+				<td colspan="99">검색결과가 없습니다</td>
+			</tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props:{
+        parentData:{
+            type: String,
+            required: false,
+            default: ""
+        },
+        paraentList:{
+            type:Array,
+            default() {
+				return [];
+			},
+        }
+    },
+    setup(props){
+        return{
+            props
+        }
+    }
+}
+</script>
+
+<style>
+.table{
+    margin: 0 auto;
+    border: 1px solid #444444;
+}
+th, td {
+    border: 1px solid #444444;
+}
+
+</style>
